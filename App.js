20c2b1a2159ff925fbcd85b29a785554af8e9a65
01me01 <96710377+01me01@users.noreply.github.com>
2022-11-24 19:11:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Profile from './components/profile'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
